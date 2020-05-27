2062cdeb0bf36018a19cdbdaa809858abfc2185d
Sue Smith <sue@benormal.info>
2020-05-27 14:48:15 +0100

    disable link checker

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,8 +54,8 @@ module.exports = {
             },
           },
           'gatsby-remark-responsive-iframe',
-          'gatsby-remark-autolink-headers',
-          'gatsby-remark-check-links'
+          'gatsby-remark-autolink-headers'/*,
+          'gatsby-remark-check-links'*/
         ],
       },
     },
