171c9ae5ce4130e4ab2cdc572322bba9d21243bc
Christina <christinavhastenrath@gmail.com>
2020-06-01 14:59:59 -0700

    adjusted Gatsby manifest to postman values

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,11 +91,11 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Postman Learning Center',
+        short_name: 'Postman Learning Center',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#FF6C37',
+        theme_color: '#FF6C37',
         display: 'minimal-ui',
         icon: 'src/images/favicon.png',
       },
