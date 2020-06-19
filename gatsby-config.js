cd6e7106d3254c4f91c466bca0edc4472f6dc9a8
Christina <christinavhastenrath@gmail.com>
2020-06-19 11:03:21 -0700

    wip remove federated search

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,23 +16,23 @@ module.exports = {
     siteUrl: 'https://learning.postman.com',
   },
   plugins: [
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'UA-43979731-4',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 1000,
-        // Defers execution of google analytics script after page load
-        defer: true,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     // The property ID; the tracking code won't be generated without it
+    //     trackingId: 'UA-43979731-4',
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: true,
+    //     // Setting this parameter is optional
+    //     anonymize: true,
+    //     // Setting this parameter is also optional
+    //     respectDNT: true,
+    //     // Delays sending pageview hits on route update (in milliseconds)
+    //     pageTransitionDelay: 1000,
+    //     // Defers execution of google analytics script after page load
+    //     defer: true,
+    //   },
+    // },
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
