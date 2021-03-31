e6946a620d680bd1c8e869ed8c6205829b02e45f
seita1996 <taha.seita@gmail.com>
2021-03-31 22:32:21 +0900

    lint error fix

--- src/components/ContextualLinks/ContextualLinks.jsx
+++ src/components/ContextualLinks/ContextualLinks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DynamicLink from '../Shared/DynamicLink';
-import recentBlogPosts from '../../../bff-data/blog.json'
+import recentBlogPosts from '../../../bff-data/blog.json';
 
 import './ContextualLinks.scss';
 
@@ -24,21 +24,25 @@ const ContextualLinks = ({ links }) => (
         }
         if (item.type === 'dynamic_blog' && item.blog_tag) {
           if (Object.keys(recentBlogPosts).length === 0) {
-            // If recentBlogPosts.length === 0, then either there is no .env.development, it has a bad blog url, or the endpoint returned something bad
+            // If recentBlogPosts.length === 0, then either there is no .env.development,
+            // it has a bad blog url, or the endpoint returned something bad
             return (
-              <div class="contextual-links__alert" role="alert">
-                <p>You are currently in develop mode. Dynamic blog posts will not be displayed locally. <a style={{"fontSize": "inherit"}}href="https://github.com/postmanlabs/postman-docs/blob/develop/CONTRIBUTING.md#to-use-the-dynamic-blog-posts-feature" target="_blank" rel="noopener noreferrer">See Contributing doc for details</a>.</p>
+              <div className="contextual-links__alert" role="alert">
+                <p>
+                  You are currently in develop mode.
+                  Dynamic blog posts will not be displayed locally.
+                  <a style={{ fontSize: 'inherit' }} href="https://github.com/postmanlabs/postman-docs/blob/develop/CONTRIBUTING.md#to-use-the-dynamic-blog-posts-feature" target="_blank" rel="noopener noreferrer">See Contributing doc for details</a>
+                  .
+                </p>
               </div>
-            )
-          } 
-          else if (recentBlogPosts.index && recentBlogPosts.index[item.blog_tag]) {
-            const dynamicLinks = recentBlogPosts.index[item.blog_tag].slice(0, 3).map(post => {
-              return (
-                <div className="contextual-links__link" key={post.title}>
-                  <DynamicLink url={post.link.url} name={post.title} />
-                </div>
-              );
-            })
+            );
+          }
+          if (recentBlogPosts.index && recentBlogPosts.index[item.blog_tag]) {
+            const dynamicLinks = recentBlogPosts.index[item.blog_tag].slice(0, 3).map((post) => (
+              <div className="contextual-links__link" key={post.title}>
+                <DynamicLink url={post.link.url} name={post.title} />
+              </div>
+            ));
             return (
               <>
                 <div className="contextual-links__subtitle" key={item.name}>
@@ -46,13 +50,15 @@ const ContextualLinks = ({ links }) => (
                 </div>
                 {dynamicLinks}
               </>
-            ) 
-          } 
-          else {
-            // If there are no related blog posts within blogs.json (written in /bff-data/ at build time), we simply return nothing.
-            // This will likely be the case when you are developing locally, and do not have a .env.development file with the BFF endpoints.
-            return;
+            );
           }
+
+          // If there are no related blog posts within blogs.json
+          // (written in /bff-data/ at build time), we simply return nothing.
+
+          // This will likely be the case when you are developing locally,
+          // and do not have a .env.development file with the BFF endpoints.
+          return null;
         }
 
         return (
