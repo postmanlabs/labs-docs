efda627489ccfa3bc6e3f64a40623da2d62ee316
brandonmcastillo <bcastillo.dev@gmail.com>
2021-04-19 15:05:28 -0700

    [MARKENG-156] Add function to start at top of new page unless window.location has a hash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,14 @@
 exports.onClientEntry = () => {
   (() => {
-    function OptanonWrapper() { } // eslint-disable-line no-unused-vars
+    function OptanonWrapper() {} // eslint-disable-line no-unused-vars
   })();
+};
+
+// Always start at the top of the page on a route change.
+exports.onInitialClientRender = () => {
+  if (!window.location.hash) {
+    window.scrollTo(0, 0);
+  } else {
+    window.location = window.location.hash;
+  }
 };
