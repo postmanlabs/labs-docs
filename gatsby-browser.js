b2cefb98e2fdcfbdcc7e303f566ea6f607b5d3ea
brandonmcastillo <bcastillo.dev@gmail.com>
2021-04-19 14:55:24 -0700

    [MARKENG-156] Remove scrollTo function in gatsby-browser causing double click issues

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,12 +3,3 @@ exports.onClientEntry = () => {
     function OptanonWrapper() { } // eslint-disable-line no-unused-vars
   })();
 };
-
-// Always start at the top of the page on a route change.
-exports.onRouteUpdate = () => {
-  if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
-}
-
-exports.shouldUpdateScroll = args => {
-   return false;
-};
