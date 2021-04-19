9481564e6255458f05d08ed62ae2b3e58ca37040
brandonmcastillo <bcastillo.dev@gmail.com>
2021-04-19 15:15:04 -0700

    Remove initialClientRender function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,12 +3,3 @@ exports.onClientEntry = () => {
     function OptanonWrapper() {} // eslint-disable-line no-unused-vars
   })();
 };
-
-// Always start at the top of the page on a route change.
-exports.onInitialClientRender = () => {
-  if (!window.location.hash) {
-    window.scrollTo(0, 0);
-  } else {
-    window.location = window.location.hash;
-  }
-};
