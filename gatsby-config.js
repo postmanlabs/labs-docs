6a17b3ff2f13ea71b2e3f644b513a3b1dba7e702
Christina <christinavhastenrath@gmail.com>
2021-08-30 10:11:25 -0700

    wip style events

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const queries = require('./src/utils/algolia');
+// const queries = require('./src/utils/algolia');
 
 // require('dotenv').config({
 //   path: `.env.${process.env.GATSBY_ACTIVE_ENV}`,
@@ -156,17 +156,17 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: 'gatsby-plugin-algolia',
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        queries,
-        chunkSize: 10000, // default: 1000
-        enablePartialUpdates: true, // only index new, changed, deleted records
-        matchFields: ['excerpt', 'contextual_links', 'search_keyword', 'headings', 'fields', 'modified'],
-        concurrentQueries: false,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-algolia',
+    //   options: {
+    //     appId: process.env.GATSBY_ALGOLIA_APP_ID,
+    //     apiKey: process.env.ALGOLIA_ADMIN_KEY,
+    //     queries,
+    //     chunkSize: 10000, // default: 1000
+    //     enablePartialUpdates: true, // only index new, changed, deleted records
+    //     matchFields: ['excerpt', 'contextual_links', 'search_keyword', 'headings', 'fields', 'modified'],
+    //     concurrentQueries: false,
+    //   },
+    // },
   ],
 };
