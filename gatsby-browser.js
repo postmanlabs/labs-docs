7856f673d675de460cf77db8d802c478bcbe44e4
Christina <christinavhastenrath@gmail.com>
2021-09-05 21:49:47 -0700

    added two new headers and dropdown functionality and algolia functionality - wip styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,12 @@
-exports.onClientEntry = () => {
-  (() => {
-    function OptanonWrapper() {} // eslint-disable-line no-unused-vars
-  })();
-};
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min';
+
+
+// exports.onClientEntry = () => {
+//   (() => {
+//     function OptanonWrapper() {} // eslint-disable-line no-unused-vars
+//   })();
+// };
+
