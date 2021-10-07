e3ffaee8ec48a0f708ebcfc1d615cdcb67a54245
Christina <christinavhastenrath@gmail.com>
2021-10-06 21:29:48 -0700

    removed commented out code

--- src/components/LeftNav/LeftNav.jsx
+++ src/components/LeftNav/LeftNav.jsx
@@ -11,10 +11,9 @@ const LeftNav = (props) => {
     document.location.href = e.target.getAttribute('data-section');
   };
   const renderTwoLevelList = (item) => {
-    // const active = document.location.pathname.match(item.parentSlug);
     const active = isRuntime ? document.location.pathname.match(item.parentSlug) : '';
-
     const indicator = (<svg className={`caret${active ? ' active-caret' : ''}`} xmlns="http://www.w3.org/2000/svg" fill="none" height="24" viewBox="0 0 24 24" width="24"><path clipRule="evenodd" d="m16.5303 8.96967c.2929.29289.2929.76777 0 1.06063l-4 4c-.2929.2929-.7677.2929-1.0606 0l-4.00003-4c-.29289-.29286-.29289-.76774 0-1.06063s.76777-.29289 1.06066 0l3.46967 3.46963 3.4697-3.46963c.2929-.29289.7677-.29289 1.0606 0z" fill="#707070" fillRule="evenodd" /></svg>);
+
     return (
       <ul key={uuidv4()}>
         <li className="parent">
