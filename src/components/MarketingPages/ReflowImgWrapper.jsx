4bc3b0b420cde6a6800663373b8a08cdf72b2297
tristandenyer <tristan@tristandenyer.com>
2021-11-11 14:38:20 -0800

    reattached the CSS to the component

--- src/components/MarketingPages/ReflowImgWrapper.jsx
+++ src/components/MarketingPages/ReflowImgWrapper.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import errorImg from '../../images/postman-404-illustration-v6.svg';
 
 const ReflowImgWrapper = () => (
-  <div className="responsive-container">
+  <div className="reflow-container">
     <img src={errorImg} alt="A bartender in space letting you know that you're on the 404 page" />
   </div>
 );
