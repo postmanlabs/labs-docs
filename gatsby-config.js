2c83ca6f9b5acca207ac38b08b9a7642f57fa1a0
brandonmcastillo <bcastillo.dev@gmail.com>
2021-12-03 14:10:22 -0800

    Gatsby 4 + Package updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-env-variables',
       options: {
-        whitelist: ['MUNCHKIN_ID'],
+        allowList: ['MUNCHKIN_ID'],
       },
     },
     {
@@ -168,23 +168,23 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-purgecss`,
+      resolve: 'gatsby-plugin-purgecss',
       options: {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         ignore: [
           '/prism-tomorrow.css',
-          '/doc.scss'
+          '/doc.scss',
         ], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-        
+
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
           safelist: [
             'collapsing',
             'gatsby-resp-iframe-wrapper',
-            /^ais-/
+            /^ais-/,
           ], // Don't remove this selector. Put regex last.
           // safelist: ['code', 'blockquote', ':not(pre)>code[class*=language-]'], // Don't remove this selector
         },
