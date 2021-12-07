34c9caacf4c0d51bf0c3591eee2addaba9a5fa47
tristandenyer <tristan@tristandenyer.com>
2021-12-07 15:15:02 -0800

    [MARKENG-1077] added print stylesheet

--- gatsby-config.js
+++ gatsby-config.js
@@ -176,6 +176,7 @@ module.exports = {
         ignore: [
           '/prism-tomorrow.css',
           '/doc.scss',
+          '/print.css',
         ], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
 
