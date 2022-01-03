bf6cd2688574c1aeb39de6a20c68b5d822e242f0
brandonmcastillo <bcastillo.dev@gmail.com>
2022-01-03 13:59:09 -0800

    [MARKENG-994] Update deeplinking scrolling issue on Docs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,19 @@ import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min';
 import './styles/config/print.css'
+
+import $ from 'jquery';
+import 'jquery.scrollto';
+
+export const onClientEntry = () => {
+  if (!window.location.hash) {
+    window.scrollTo(0, 0);
+  } else {
+    // Race condition with default browser behavior for deep links.
+    setTimeout(() => {
+      $.scrollTo($(window.location.hash), {
+        offset: { top: -80 }
+      });
+    }, 1000)
+  }
+}
