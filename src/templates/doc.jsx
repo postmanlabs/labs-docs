09ca60b2f615dae561582d81fce8284ba1f82625
brandonmcastillo <bcastillo.dev@gmail.com>
2022-01-11 14:49:23 -0800

    Update edge case bug for "Last modified" date

--- src/templates/doc.jsx
+++ src/templates/doc.jsx
@@ -35,7 +35,9 @@ const DocPage = ({ data }) => {
               <main className="col-sm-12 col-md-12 col-lg-9 offset-lg-0 col-xl-7 doc-page ml-xl-5">
                 <h1>{post.frontmatter.title}</h1>
                 <span dangerouslySetInnerHTML={{ __html: post.html }} />
-                <small className="font-italic">Last modified: {date}</small>
+                <p>
+                  <small className="font-italic">Last modified: {date}</small>
+                </p>
               </main>
               <aside className="col-sm-12 col-md-12 col-lg-3 offset-lg-0 col-xl-3 offset-xl-1 right-column">
                 <hr className="d-block d-lg-none" />
