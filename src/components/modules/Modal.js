f640649246b0eeb6b1a8bbccc1930153c8d6c1d7
brandonmcastillo <bcastillo.dev@gmail.com>
2022-02-18 12:16:55 -0800

    Added click out for modal

--- src/components/modules/Modal.js
+++ src/components/modules/Modal.js
@@ -23,42 +23,23 @@ const Modal = (create_id, images, i) => {
       <img src=${images[i].src || null} alt=${images[i].alt || null}></img>
     </a>
     <div id=${create_id} class="modal modal-link fade" tab-index="-1" role="dialog" aria-labelledby=${images[i].alt || null} aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-close-button">
-          <button
-            type="button"
-            class="close"
-            data-dismiss="modal"
-            aria-label="close"
-          >
-            <span
-              class="closed-icon"
-              aria-hidden="true"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-              >
-                <title class="closed-label">e-remove</title>
-                <g
-                  stroke-width="1"
-                  fill="#212121"
-                  stroke="#212121"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <line x1="13.5" y1="2.5" x2="2.5" y2="13.5"></line>
-                  <line x1="2.5" y1="2.5" x2="13.5" y2="13.5"></line>
-                </g>
-              </svg>
-            </span>
-              </button>
-            
-            </div>
-            <img src=${images[i].src || null}  alt=${images[i].alt || null}></img>
+      <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-close-button">
+      <button
+        type="button"
+        class="close"
+        data-dismiss="modal"
+        aria-label="close"
+      >
+      <div class="container">
+
+      <div class="modal-content row">
+      <div class="col">
+        <img class="img-fluid" src=${images[i].src || null}  alt=${images[i].alt || null}></img>
+      </div>
+      </div>
+        </div>
+        </button>     
         </div>
       </div>
     </div>
