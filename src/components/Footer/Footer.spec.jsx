f80309e85ba041976f7e2519237d61f2ffc6e006
Brandon Castillo <bcastillo.dev@gmail.com>
2022-04-20 12:41:38 -0700

    Update packages and jest configuration

--- src/components/Footer/Footer.spec.jsx
+++ src/components/Footer/Footer.spec.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Footer from './Footer';
 
+describe('Foother', () => {
 it('renders correctly', () => {
   const tree = renderer
     .create(<Footer />)
     .toJSON();
   expect(tree).toMatchSnapshot();
+  });
 });
