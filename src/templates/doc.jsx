212cbbb01ccb0c7962ff64ee183d6776b5ce1860
sterling.chin <sterling.chin@postman.com>
2022-08-30 16:59:18 -0500

    add alert at the top of all Labs docs to let customers know it is all experiemental and subject to change

--- src/templates/doc.jsx
+++ src/templates/doc.jsx
@@ -64,6 +64,10 @@ const DocPage = ({ data }) => {
             <div className="row row-eq-height">
               <main className="col-sm-12 col-md-12 col-lg-9 offset-lg-0 col-xl-7 doc-page ml-xl-5">
                 <BreadCrumbsLinks data={{ parentLink, subParentLink }} />
+                <div className="experimental-alert">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"/></svg>
+                  <i>All content is experimental and subject to change</i>
+                </div>
                 <h1>{post.frontmatter.title}</h1>
                 <CreateDoc data={post} />
                 <p>
