c59c76c35babaff48c85d87a32393014b9699542
OlliesWorld <rtlockwoodwork@gmail.com>
2023-03-31 14:00:56 -0600

    [MARKENG-2349] update SEO with pathPrefix and 404 with correct paths

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,7 +6,7 @@ import ReflowImgWrapper from '../components/MarketingPages/ReflowImgWrapper';
 
 const NotFoundPage = () => (
    <Layout>
-      <SEO title="404: Not found" slug="/404/" />
+      <SEO title="404: Not found" slug="/labs/404/" />
       <div className="container" role="link" tabIndex={0}>
         <div style={{ paddingTop: '80' + 'px', paddingBottom: '80' + 'px' }} className="row">
           <div className="col-md-8 align-self-center pr-md-5">
@@ -15,7 +15,7 @@ const NotFoundPage = () => (
           <div className="col-md-4 align-self-center">
             <h1>404</h1>
             <p className="subtitle">Oops! We can&apos;t find this page. It may have been moved or deleted.</p>
-            <a href="/" className="btn btn__primary-hollow">Back to Docs</a>
+            <a href="/labs/labs-overview/" className="btn btn__primary-hollow">Back to Docs</a>
           </div>
         </div>
       </div>
